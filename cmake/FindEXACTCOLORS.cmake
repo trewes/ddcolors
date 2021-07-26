85be6b050b48b702543bea820e3ba8fe5e4aca2b
trewes <63164511+trewes@users.noreply.github.com>
2021-07-26 12:59:28 +0200

    Final commit for my bachelor thesis.

--- cmake/FindEXACTCOLORS.cmake
+++ cmake/FindEXACTCOLORS.cmake
@@ -9,43 +9,41 @@
 
 # use given hint directory or look in parent/exactcolors folder
 IF(NOT DEFINED EXACTCOLORS_ROOT_DIR)
-    GET_FILENAME_COMPONENT(DIR_ONE_ABOVE ../ ABSOLUTE)
-    SET(EXACTCOLORS_ROOT_DIR ${DIR_ONE_ABOVE}/exactcolors)
+    SET(EXACTCOLORS_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../exactcolors)
 ENDIF()
 
-
 FIND_PATH(EXACTCOLORS_INCLUDE_DIR lp.h
-        PATH_SUFFIXES include
-        PATHS ${EXACTCOLORS_ROOT_DIR}
-        )
+          PATH_SUFFIXES include
+          PATHS ${EXACTCOLORS_ROOT_DIR}
+          )
 MESSAGE(STATUS "EXACTCOLORS Include Dir: ${EXACTCOLORS_INCLUDE_DIR}")
 
 FIND_PATH(EXACTCOLORS_MWIS_SEWELL_DIR mwss.h
-        PATH_SUFFIXES include
-        HINTS ${EXACTCOLORS_ROOT_DIR}/mwis_sewell
-        PATHS ${EXACTCOLORS_ROOT_DIR}
-        )
+          PATH_SUFFIXES include
+          HINTS ${EXACTCOLORS_ROOT_DIR}/mwis_sewell
+          PATHS ${EXACTCOLORS_ROOT_DIR}
+          )
 MESSAGE(STATUS "MWIS SEWELL Include Dir: ${EXACTCOLORS_MWIS_SEWELL_DIR}")
 
 FIND_LIBRARY(EXACTCOLORS_LIBRARY
-        NAMES exactcolor
-        PATH_SUFFIXES lib
-        PATHS ${EXACTCOLORS_ROOT_DIR}
-        )
+             NAMES exactcolor libexactcolor.a
+             PATH_SUFFIXES lib
+             PATHS ${EXACTCOLORS_ROOT_DIR}
+             )
 MESSAGE(STATUS "EXACTCOLORS Library: ${EXACTCOLORS_LIBRARY}")
 
 FIND_LIBRARY(EXACTCOLORS_SEWELL_LIBRARY
-        NAMES sewell
-        PATH_SUFFIXES lib
-        HINTS ${EXACTCOLORS_ROOT_DIR}/mwis_sewell
-        PATHS ${EXACTCOLORS_ROOT_DIR}
-        )
+             NAMES sewell libsewell.a
+             PATH_SUFFIXES lib
+             HINTS ${EXACTCOLORS_ROOT_DIR}/mwis_sewell
+             PATHS ${EXACTCOLORS_ROOT_DIR}
+             )
 MESSAGE(STATUS "SEWELL Library: ${EXACTCOLORS_SEWELL_LIBRARY}")
 
 # check whether required things have been found and set EXACTCOLORS_FOUND accordingly
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(EXACTCOLORS DEFAULT_MSG
-        EXACTCOLORS_INCLUDE_DIR EXACTCOLORS_MWIS_SEWELL_DIR EXACTCOLORS_LIBRARY EXACTCOLORS_SEWELL_LIBRARY)
+                                  EXACTCOLORS_INCLUDE_DIR EXACTCOLORS_MWIS_SEWELL_DIR EXACTCOLORS_LIBRARY EXACTCOLORS_SEWELL_LIBRARY)
 
 # if package has been found, collect include dirs and libraries
 IF(EXACTCOLORS_FOUND)
